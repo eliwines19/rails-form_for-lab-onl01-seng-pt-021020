aaf1c27a6d2dc658c3ac979b1ff875ddaf6750be
Elijah wines <elijahwines84@yahoo.com>
2020-06-17 19:57:07 +0000

    Automatically backed up by Learn

--- app/controllers/student_controller.rb
+++ app/controllers/student_controller.rb
@@ -1,22 +1,14 @@
 class StudentsController < ApplicationController
-
   def index
-    @students = Student.all
   end
 
   def show
-    @student = Student.find(params[:id])
   end
 
   def new
-    @student = Student.last
   end
 
   def create
-    @student = Student.new
-    @student.first_name = params[:first_name]
-    @student.last_name = params[:last_name]
-    @student.save
-    redirect_to student_path(@student)
   end
+
 end
