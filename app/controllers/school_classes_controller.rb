e0c293a485abee5ae27386ec5ded843f016b2b74
Elijah wines <elijahwines84@yahoo.com>
2020-06-17 20:15:00 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -11,10 +11,7 @@ class SchoolClassesController < ApplicationController
   end
 
   def create
-    @school_class = SchoolClass.new
-    @school_class.title = params[:title]
-    @school_class.room_number = params[:room_number]
-    @school_class.save
+    @school_class = SchoolClass.create(strong_params(:title, :room_number))
     redirect_to school_class_path(@school_class)
   end
 
