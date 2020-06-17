b3b6c0393c44deab825cec4e1f9a47b059103d8f
Elijah wines <elijahwines84@yahoo.com>
2020-06-17 20:17:46 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,13 +8,11 @@ class StudentsController < ApplicationController
   end
 
   def new
+    @student = Student.new
   end
 
   def create
-    @student = Student.new
-    @student.first_name = params[:first_name]
-    @student.last_name = params[:last_name]
-    @student.save
+    @student = Student.create(strong_params(:first_name, :last_name))
     redirect_to student_path(@student)
   end
 
