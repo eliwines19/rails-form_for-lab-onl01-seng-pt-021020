dde4bd8dcf09ab0e2b228238f4f6c06732b76dcb
Elijah wines <elijahwines84@yahoo.com>
2020-06-17 20:05:48 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,13 +1,20 @@
 class SchoolClassesController < ApplicationController
   def index
+    @class = SchoolClasses.all
   end
 
   def show
+    @class = SchoolClasses.find(params[:id])
   end
 
   def new
   end
 
   def create
+    @class = SchoolClasses.new
+    @class.title = params[:title]
+    @class.room_number = params[:room_number]
+    @class.save
+    redirect_to school_class_path(@class)
   end
 end
